4620f6a1e12f58c5d4bfa62cdd564a8e4d331cdd
SIGMAxox <ahmedger63@gmail.com>
2026-09-01 00:37:30 +0300

    Update AttackMixin.java

--- src/main/java/com/sigma/sigmamc/mixin/AttackMixin.java
+++ src/main/java/com/sigma/sigmamc/mixin/AttackMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class AttackMixin {
@@ -14,7 +14,7 @@ public class AttackMixin {
             method = "doAttack",
             at = @At("HEAD")
     )
-    private void sigma_onAttack(CallbackInfo ci) {
+    private void sigma_onAttack(CallbackInfoReturnable<Boolean> cir) {
 
         System.out.println(
                 "[Sigma_mc] LEFT CLICK DETECTED"
