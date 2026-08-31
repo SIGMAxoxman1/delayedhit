c83bc99ca3e3b4ccc6277e93e4d1835909407d3f
SIGMAxox <ahmedger63@gmail.com>
2026-08-31 18:37:53 +0300

    Implement player name logging on client tick
    Add client tick event to log player name every 20 ticks.

--- src/main/java/com/sigma/sigmamc/SigmaMcClient.java
+++ src/main/java/com/sigma/sigmamc/SigmaMcClient.java
@@ -1,10 +1,31 @@
 package com.sigma.sigmamc;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 
 public class SigmaMcClient implements ClientModInitializer {
+
+    private static int ticks = 0;
+
     @Override
     public void onInitializeClient() {
+
         System.out.println("[Sigma_mc] Loaded");
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            ticks++;
+
+            if (ticks >= 20) {
+                ticks = 0;
+
+                if (client.player != null) {
+                    System.out.println(
+                            "[Sigma_mc] Player: "
+                                    + client.player.getName().getString()
+                    );
+                }
+            }
+        });
     }
 }
